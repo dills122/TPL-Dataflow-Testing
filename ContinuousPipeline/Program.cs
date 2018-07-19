6992d8a395537347a97d8ee7c898633366794e42
Dylan Steele <dylansteele57@gmail.com>
2018-07-18 20:15:41 -0400

    Posting multiple items and returning when all have finished. Reads input string and outputs the string reversed.

--- ContinuousPipeline/Program.cs
+++ ContinuousPipeline/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
 
@@ -9,30 +11,42 @@ namespace ContinuousPipeline
         public static BufferBlock<InputClass> bufferBlock;
         public static ActionBlock<InputClass> outputBlock;
         public static TransformBlock<InputClass, InputClass> transformBlock;
+        public static List<string> processedInfo;
 
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
 
-            Run().Wait();
+            //Run().Wait();
+
+            List<InputClass> inputClasses = new List<InputClass>();
+            for (int i = 0; i < 10; i++)
+            {
+                inputClasses.Add(new InputClass());
+            }
+            PostPipeline(inputClasses).Wait();
+            foreach(string str in processedInfo)
+            {
+                Console.WriteLine("Recieved Posted info: {0}", str);
+            }
             Console.WriteLine("Finished");
             Console.ReadKey();
         }
 
-        public static async Task Run()
-        {
-            BuildPipeline();
-            InputClass inputClass = new InputClass
-            {
-                inputString = "string input",
-                transformString = "",
-                Completion = new TaskCompletionSource<string>()
-            };
-            bufferBlock.Post(inputClass);
+        //public static async Task Run()
+        //{
+        //    BuildPipeline();
+        //    InputClass inputClass = new InputClass
+        //    {
+        //        inputString = "string input",
+        //        transformString = "",
+        //        Completion = new TaskCompletionSource<string>()
+        //    };
+        //    bufferBlock.Post(inputClass);
 
-            var test = inputClass.Completion.Task;
-            Console.WriteLine("After Pipeline: {0}",test.Result);
-        }
+        //    var test = inputClass.Completion.Task;
+        //    Console.WriteLine("After Pipeline: {0}",test.Result);
+        //}
 
         public static void BuildPipeline()
         {
@@ -40,14 +54,15 @@ namespace ContinuousPipeline
 
             transformBlock = new TransformBlock<InputClass, InputClass>(input =>
             {
-                input.transformString = "Transformed at " + DateTime.Now;
+                Console.WriteLine("Received String: {0}", input.inputString);
+                input.inputString = new string(input.inputString.ToCharArray().Reverse().ToArray());
                 return input;
             });
 
             outputBlock = new ActionBlock<InputClass>(input =>
             {
-                Console.WriteLine("Output: {0}", input.transformString);
-                input.Completion.SetResult(input.transformString);
+                //Console.WriteLine("Read for output: {0}", input.inputString);
+                input.Completion.SetResult(input.inputString);
             });
 
             var options = new DataflowLinkOptions { PropagateCompletion = true };
@@ -56,13 +71,39 @@ namespace ContinuousPipeline
             transformBlock.LinkTo(outputBlock, options);
         }
 
+        public static Task PostPipeline(List<InputClass> inputClasses)
+        {
+            BuildPipeline();
+            processedInfo = new List<string>();
+            foreach(InputClass input in inputClasses)
+            {
+                bufferBlock.Post(input);
+
+                processedInfo.Add(input.Completion.Task.Result);
+            }
+            return Task.CompletedTask;
+        }
+
+        private static Random random = new Random();
+        public static string RandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+            return new string(Enumerable.Repeat(chars, length)
+              .Select(s => s[random.Next(s.Length)]).ToArray());
+        }
+
     }
 
     public class InputClass
     {
         public string inputString { get; set; }
-        public string transformString { get; set; }
         
         public TaskCompletionSource<string> Completion { get; set; }
+
+        public InputClass()
+        {
+            inputString = Program.RandomString(25);
+            Completion = new TaskCompletionSource<string>();
+        }
     }
 }
